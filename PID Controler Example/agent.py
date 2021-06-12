a4272ad814996d13898613a21784233688ce0a47
luisgaboardi <luis.ggaboardi@gmail.com>
2021-06-11 22:26:03 -0300

    Integration of the agent, pid_controller and route planner

--- PID Controler Example/agent.py
+++ PID Controler Example/agent.py
@@ -1,3 +1,17 @@
+import glob
+import os
+import sys
+
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+import carla
+
+from pid_controller import VehiclePIDController
 from route_planner import RoutePlanner
 
 
@@ -6,12 +20,48 @@ class Agent():
     def __init__(self, vehicle):
         self.vehicle = vehicle
         self.world = vehicle.get_world()
+        self.map = self.world.get_map()
+        self.control_vehicle = VehiclePIDController(vehicle,
+                                                    args_lateral={
+                                                        'K_P': 0.58, 'K_D': 0.02, 'K_I': 0.5},
+                                                    args_longitudinal={
+                                                        'K_P': 0.15, 'K_D': 0.05, 'K_I': 0.07}
+                                                    )
         self.spawn_point = None
         self.destination_point = None
         self.route_planner = None
+        self.route = []
 
     def get_route(self, spawn_point, destination_point, debug=True):
         self.spawn_point = spawn_point
         self.destination_point = destination_point
-        self.route_planner = RoutePlanner(self.world, self.spawn_point, self.destination_point)
-        return self.route_planner.generate_route(debug=debug)
+        self.route_planner = RoutePlanner(
+            self.world, self.spawn_point, self.destination_point)
+        self.route = self.route_planner.generate_route(debug=debug)
+        return self.route
+
+    def soft_stop(self):
+        control = carla.VehicleControl()
+        control.steer = 0.0
+        control.throttle = 0.0
+        control.brake = 0.65
+        control.hand_brake = False
+
+        return control
+
+    def equal_location(self, vehicle, waypoint):
+	    if((abs(vehicle.get_location().x - waypoint.transform.location.x) <= 0.5
+	    and abs(vehicle.get_location().y - waypoint.transform.location.y) <= 0.5)):
+		    return True
+	    return False
+
+    def run_step(self):
+        print("Oi")
+        while not self.equal_location(self.vehicle, self.route[0]): 
+            control = self.control_vehicle.run_step(self.vehicle.get_speed_limit(), self.route[0])
+            self.vehicle.apply_control(control)
+
+        self.route.pop(0)
+
+    def arrived(self):
+        return len(self.route) <= 5
--- PID Controler Example/travel_route_with_PID.py
+++ PID Controler Example/travel_route_with_PID.py
@@ -12,7 +12,7 @@ except IndexError:
     pass
 import carla
 
-from pid_controller import VehiclePIDController
+
 from agent import Agent
 
 
@@ -36,21 +36,18 @@ def main():
         sleep(1)
 
         world.get_spectator().set_transform(vehicle.get_transform())
-
-        control_vehicle = VehiclePIDController(vehicle,
-                            args_lateral={'K_P': 0.58, 'K_D': 0.02, 'K_I': 0.5},
-                            args_longitudinal={'K_P': 0.15, 'K_D': 0.05, 'K_I': 0.07,}
-                        )
-
         agent = Agent(vehicle)
-        route = agent.get_route(spawn_point, destination_point, True)
+        agent.get_route(spawn_point, destination_point)
 
-        for wp in route:
-            while not ((abs(vehicle.get_location().x - wp.transform.location.x) <= 5
-	                    and abs(vehicle.get_location().y - wp.transform.location.y) <= 5)):
-                control_signal = control_vehicle.run_step(vehicle.get_speed_limit(), wp)
-                vehicle.apply_control(control_signal)
+        while (True):
 
+            if agent.arrived():
+                vehicle.apply_control(agent.soft_stop())
+                print("Target reached, mission accomplished...")
+                sleep(3)
+                break
+
+            agent.run_step()
 
 
     finally:
